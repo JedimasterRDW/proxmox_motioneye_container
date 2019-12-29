97e0f9f68b7d77b2ffd7fe07bd10807ab06fc70e
JedimasterRDW <58998478+JedimasterRDW@users.noreply.github.com>
2019-12-29 21:07:38 +0100

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -112,7 +112,7 @@ HOSTNAME=motioneye
 TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
 pct create $CTID $TEMPLATE_STRING -arch $ARCH -cores 1 -hostname $HOSTNAME \
   -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -ostype $OSTYPE \
-  -password "motioneye" -rootfs $ROOTFS -storage $STORAGE -unprivileged 1 >/dev/null
+  -password "motioneye" -rootfs $ROOTFS -storage $STORAGE --unprivileged 1 >/dev/null
 
 # Set container timezone to match host
 MOUNT=$(pct mount $CTID | cut -d"'" -f 2)
