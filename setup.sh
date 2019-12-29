77aa077e17ece4146894f2276f997111f14037e5
JedimasterRDW <58998478+JedimasterRDW@users.noreply.github.com>
2019-12-29 23:56:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ export PATH=$PATH:/usr/local/bin
 # Install MotionEye
 msg "Installing MotionEye..."
 # yes 2>/dev/null | 
-pip install motioneye >/dev/null
+pip install motioneye &>/dev/null
 
 # Prepare the configuration
 msg "Prepare the configuration..."
